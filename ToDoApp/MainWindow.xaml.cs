629f253ffe02aea3827aa1db0e031c474be753d5
Damond LOKS <super.dimasik53@yandex.ru>
2020-09-14 20:05:27 +0300

    GUI C# APP

---
+++ ToDoApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoApp.NewFolder;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BindingList<TodoModel> _todoData;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)//загрузка в модель из файла
+        {
+            _todoData = new BindingList<TodoModel>()
+            {
+            new TodoModel{myBusines="Test busines",Done=false}
+            };
+            ToDo_List.ItemsSource = _todoData;
+        }
+    }
+}
---
+++ ToDoApp/NewFolder/TodoModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Timers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.NewFolder
+{
+    public class TodoModel
+    {
+        public System.DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _Done;
+
+        public bool Done
+        {
+            set
+            {
+                _Done = value;
+            }
+            get
+            {
+                return _Done;
+            }
+        }
+
+
+        private string _myBusines;
+
+        public string myBusines
+        {
+            set
+            {
+                _myBusines = value;
+            }
+            get
+            {
+                return _myBusines;
+            }
+        }
+
+    }
+}
